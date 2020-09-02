9ecab72f4e138c5bda71179f41aeac24ad2f4693
Ricardo Gallegos <ricardogallegos93@DESKTOP-GV89BSD.localdomain>
2020-09-02 11:58:03 -0600

    Committer: Ricardo Gallegos <ricardogallegos93@DESKTOP-GV89BSD.localdomain>

--- conexion.php
+++
@@ -1,7 +0,0 @@
-<?php 
-	function conecta(){
-		$link = mysqli_connect("localhost", "root", "", "proyecto");
-		return $link;
-	}
-
- ?>
--- seguridad.php
+++
@@ -1,8 +0,0 @@
-<?php
-@session_start();
-if($_SESSION["autentica"] != "SI")
-{
-	header("Location:index.php");
-	exit();
-}
-  ?>
